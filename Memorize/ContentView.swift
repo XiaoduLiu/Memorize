1eda0f4c4b4953d3dcc8e4272a36ea75295db255
“Louie <liuxiaodu@yahoo.com>
2023-11-05 15:30:55 -0800

    update

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         HStack {
-            CardView()
+            CardView(isFaceUp: true)
             CardView()
             CardView()
             CardView()
@@ -22,13 +22,18 @@ struct ContentView: View {
 }
 
 struct CardView: View {
+    var isFaceUp: Bool = false
     var body: some View {
         ZStack (content: {
-            RoundedRectangle(cornerRadius: 12)
-                .foregroundColor(.white)
-            RoundedRectangle(cornerRadius: 12.0)
-                .strokeBorder(lineWidth: 2)
-            Text("🚗").font(.largeTitle)
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12.0)
+                    .strokeBorder(lineWidth: 2)
+                Text("🚗").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 12)
+            }
         })
     }
 }
