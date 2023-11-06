a3bfa0706925ab730780d0fb00245931911f0d8d
“Louie <liuxiaodu@yahoo.com>
2023-11-05 16:12:42 -0800

    update

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -25,6 +25,7 @@ struct CardView: View {
     var isFaceUp: Bool = true
     var body: some View {
         ZStack (content: {
+            //can only do local variable, if-else and list of views
             var base: RoundedRectangle = RoundedRectangle(cornerRadius: 12)
             if isFaceUp {
                 base.fill(.white)
