e475bdd48185b688c7ddc3091281752e91ec857d
“Louie <liuxiaodu@yahoo.com>
2023-11-07 16:38:23 -0800

    model

---
+++ Memorize/MemorizeGame.swift
@@ -0,0 +1,23 @@
+//
+//  MemorizeGame.swift
+//  Memorize
+//
+//  Created by Louie Liu on 11/7/23.
+//
+
+import Foundation
+
+struct MemorizeGame<CardContent> {
+    
+    var cards: Array<Card>
+    
+    func choose(card: Card) {
+        
+    }
+    
+    struct Card {
+        var isFaceUp: Bool
+        var isMatched: Bool
+        var content: CardContent
+    }
+}
