2757a2c63ae3bdf76aadc2dc9cdaaaaa7041a48b
“Louie <liuxiaodu@yahoo.com>
2023-11-12 15:33:45 -0800

    no need to return if only one line of code

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -17,25 +17,56 @@ struct EmojiMemoryGameView: View {
             ScrollView {
                 cards
                     .animation(.default,value: viewModel.cards)
+                    //.background(Color.red)
             }
             Button("Shuffle") {
                 viewModel.shuffle()
             }
+            //.background(Color.blue)
         }
         .padding()
+        //.background(Color.yellow)
     }
     var cards: some View {
-        LazyVGrid(columns: [GridItem(.adaptive(minimum: 85), spacing: 0)], spacing: 0) {
-            ForEach(viewModel.cards) { card in
-                CardView(card)
-                    .aspectRatio(2/3, contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
-                    .padding(4)
-                    .onTapGesture {
-                        viewModel.choose(card)
-                    }
+        GeometryReader { geometry in
+            let gridItemSize = gridItemWidthThatFits(
+                count: viewModel.cards.count,
+                size: geometry.size,
+                atAspectRatio: 2/3
+            )
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: gridItemSize), spacing: 0)], spacing: 0) {
+                ForEach(viewModel.cards) { card in
+                    CardView(card)
+                        .aspectRatio(2/3, contentMode: .fit)
+                        .padding(4)
+                        .onTapGesture {
+                            viewModel.choose(card)
+                        }
+                }
             }
         }
         .foregroundColor(.orange)
+    }
+        
+    func gridItemWidthThatFits(
+        count: Int,
+        size: CGSize,
+        atAspectRatio aspectRatio: CGFloat
+    ) -> CGFloat {
+        let count = CGFloat(count)
+        var columnCount = 1.0
+        repeat {
+            let width = size.width / columnCount
+            let height = width / aspectRatio
+                    
+            let rowCount = (count / columnCount).rounded(.up)
+            if rowCount * height < size.height {
+                //return (size.width / columnCount).rounded(.down)
+            }
+            columnCount += 1
+        } while columnCount < count
+        //return min(size.width / count, size.height * aspectRatio).rounded(.down)
+        return 65
     }
 
 }
