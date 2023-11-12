bbcb0f8a1e9d39876667f70d47dbd19348058152
“Louie <liuxiaodu@yahoo.com>
2023-11-11 17:23:22 -0800

    use option for index

--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -27,17 +27,19 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
     mutating func choose(_ card: Card) {
         //print("choose \(card)")
         //card.isFaceUp.toggle() (wrong)
-        let chosenIndex = index(of: card)
-        cards[chosenIndex].isFaceUp.toggle()
+        if let chosenIndex = index(of: card) {
+            cards[chosenIndex].isFaceUp.toggle()
+        }
+        
     }
     
-    func index(of card: Card) -> Int {
+    func index(of card: Card) -> Int? {
         for index in cards.indices {
             if cards[index].id == card.id {
                 return index
             }
         }
-        return 0 //FIXME: bogus!
+        return nil
     }
     
     //otherwise, it will not allow to shuffle self is immutable
