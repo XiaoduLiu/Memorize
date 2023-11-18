0e2aae29a84309f32a6130e9162444afaf3e0186
“Louie <liuxiaodu@yahoo.com>
2023-11-18 14:55:05 -0800

    start to draw a shap

--- Memorize/CardView.swift
+++ Memorize/CardView.swift
@@ -22,12 +22,22 @@ struct CardView: View {
             Group {
                 base.fill(.white)
                 base.strokeBorder(lineWidth: Constants.lineWidth)
-                Text(card.content)
-                    .font(.system(size:Constants.FountSize.largest))
-                    .minimumScaleFactor(Constants.FountSize.scaleFactory)
-                    .aspectRatio(1, contentMode: .fit)
-                    .multilineTextAlignment(.center)
-                    .padding(Constants.inset)
+                Path { p in
+                    p.move(to: .zero)
+                    p.addLine(to: CGPoint(x: 50, y: 200))
+                }
+                .stroke(lineWidth: 6)
+                Pie(endAngle: .degrees(240))
+                    .opacity(Constants.Pie.opacity)
+                    .overlay (
+                            Text(card.content)
+                                .font(.system(size:Constants.FountSize.largest))
+                                .minimumScaleFactor(Constants.FountSize.scaleFactory)
+                                .aspectRatio(1, contentMode: .fit)
+                                .multilineTextAlignment(.center)
+                                .padding(Constants.Pie.padding)
+                )
+                .padding(Constants.inset)
             }
             .opacity(card.isFaceUp ? 1 : 0)
             base.fill().opacity(card.isFaceUp ? 0 : 1)
@@ -43,6 +53,10 @@ struct CardView: View {
             static let largest: CGFloat = 200
             static let smallest: CGFloat = 10
             static let scaleFactory = smallest / largest
+        }
+        struct Pie {
+            static let padding: CGFloat = 5
+            static let opacity: CGFloat = 0.4
         }
     }
 }
